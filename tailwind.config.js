0ed26bc2ce078e055668ab5bc2c1d501d7b1a5ec
JoseMa <esparev@hotmail.com>
2023-06-13 14:23:07 -0600

    style: 🎨 agregar estilos personalizados

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
-/** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
@@ -7,12 +8,25 @@ module.exports = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ['Inter', ...defaultTheme.fontFamily.sans],
+        poppins: ['Poppins', ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        'gray-800': '#262626',
+        'gray-900': '#171717',
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      animation: {
+        'spin-slow': 'spin 5s linear infinite',
+        'spin-slower': 'spin 6s linear infinite',
+        'spin-reverse-slower': 'spin 6s linear infinite reverse',
+      },
+      // TODO: add transition-colors
     },
   },
   plugins: [],
-}
+};
