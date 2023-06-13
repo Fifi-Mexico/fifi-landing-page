cdf6b5ffe01d2ed166310a8ae8f1d5929879bda6
JoseMa <esparev@hotmail.com>
2023-06-13 16:53:07 -0600

    seo: 🔍️ agregar google analytics

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,18 @@ import { Html, Head, Main, NextScript } from 'next/document';
 export default function Document() {
   return (
     <Html lang='es-MX'>
-      <Head />
+      <Head>
+        <script async src='https://www.googletagmanager.com/gtag/js?id=G-EZWF1W7RHN'></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                  
+                    gtag('config', 'G-EZWF1W7RHN');`,
+          }}
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
