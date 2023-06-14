1b09485d5530ac6534e1a741fc63ba63d1d9e343
JoseMa <esparev@hotmail.com>
2023-06-13 22:12:54 -0600

    seo: 🔍️ insertar script para cookie hub

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -15,6 +15,17 @@ export default function Document() {
             gtag('config', 'G-EZWF1W7RHN');
           `}
         </Script>
+        <Script id='cookie-hub' strategy='afterInteractive'>
+          {`
+            var cpm = {};
+            (function(h,u,b){
+              var d=h.getElementsByTagName("script")[0],e=h.createElement("script");
+              e.async=true;e.src='https://cdn.cookiehub.eu/c2/7664d83d.js';
+              e.onload=function(){u.cookiehub.load(b);};
+              d.parentNode.insertBefore(e,d);
+            })(document,window,cpm);
+          `}
+        </Script>
       </Head>
       <body>
         <Main />
